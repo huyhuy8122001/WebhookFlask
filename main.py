1c5cbe354110bb6a967a1c6ba384d684b12e9191
HuyQuangNguyen <huyquangng258@gmail.com>
2023-10-10 00:37:06 +0700

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", method=["POST"])
+@app.route("/", methods=["POST"])
 def hook():
     print(request.data)
     return "Hello World"

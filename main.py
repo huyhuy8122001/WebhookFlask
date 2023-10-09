342f6661ab33bff53840da99a018747455b99771
root <root@fusionpbx>
2023-10-09 19:36:05 +0000

    update

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
 from flask import Flask, request
+import requests
+import json
+
+DATABASE_TOKEN = "iFgDQSuA9HIKiUyF9G2oo8I099cIaPJu"
+
+def create_row_baserow(timestamp, event_name, sender_id, recipient_id, text):
+    url = "http://178.128.110.168:8080/api/database/rows/table/1149/?user_field_names=true"
+
+    header = {
+        "Authorization": "Token iFgDQSuA9HIKiUyF9G2oo8I099cIaPJu",
+        "Content-Type": "application/json"
+    }
+
+    data = { 
+        "timestamp": timestamp,
+        "event_name": event_name,
+        "sender_id": sender_id,
+        "recipient_id": recipient_id,
+        "text": text
+        }
+
+    requests.post(
+        url,
+        headers=header,
+        json= data
+    )
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def hook():
-    print(request.data)
+    request_data = request.data
+    print(request_data)
+    print(type(request_data))
+    request_data_dict = json.loads(request_data.decode('utf-8'))
+    print(type(request_data_dict))
+    
+    timestamp = request_data_dict['timestamp']
+    event_name = request_data_dict['event_name']
+    sender_id = request_data_dict['sender']['id']
+    recipient_id = request_data_dict['recipient']['id']
+    text = request_data_dict['message']['text']
+    
+    create_row_baserow(timestamp, event_name, sender_id, recipient_id, text)
+    
     return "Hello World"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0')
--- send_request.py
+++ send_request.py
@@ -1,7 +1,27 @@
 import requests
 
-#url = "http://127.0.0.1:5000/"
-url = "http://178.128.110.168:5000/"
+# url = "http://127.0.0.1:5000/"
 
-r = requests.post(url)
-print(r.content)
+# r = requests.post(url)
+# print(r)
+
+url = "http://178.128.110.168:8080/api/database/rows/table/1149/?user_field_names=true"
+
+header = {
+    "Authorization": "Token iFgDQSuA9HIKiUyF9G2oo8I099cIaPJu",
+    "Content-Type": "application/json"
+}
+
+data = { 
+    "timestamp": 0,
+    "event_name": "string",
+    "sender_id": 0,
+    "recipient_id": 0,
+    "text": "string"
+    }
+
+requests.post(
+    url,
+    headers=header,
+    json= data
+)

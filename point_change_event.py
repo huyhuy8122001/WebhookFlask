cb532094d4f9a92624aa7c2559506ac52a7c0c8d
huyhuy8122001 <huyhuy8122001@gmail.com>
2023-10-20 16:44:25 +0700

    update

--- point_change_event.py
+++ point_change_event.py
@@ -5,8 +5,8 @@ import json
 DATABASE_TOKEN = "A2WseJWQzGQmTKWv540A1PnsJp3PU7ms"
 
 host = "http://report.hq-devlab.cloud:8080"
-table_id = 869
-field_id = 'field_7854'
+table_id = 836
+field_id = 'field_7506'
 
 def search_row_id(contact_id):
     url = f"{host}/api/database/rows/table/{table_id}/?user_field_names=true&filter__{field_id}__equal={contact_id}"
